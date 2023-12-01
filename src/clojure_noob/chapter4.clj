3bc070bfba1be3ae4fdb268c56059c2fb42f0a10
Tri <tri.nguyen@gss-sol.com>
2023-12-01 17:25:58 +0700

    Finish Seq functions

---
+++ src/clojure_noob/chapter4.clj
@@ -0,0 +1,26 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+
+(drop-while #(< (:month %) 3) food-journal)
+
+(filter #(< (:human %) 5) food-journal)
+
+(filter #(and (> (:month %) 1)
+              (< (:month %) 4))
+        food-journal)
+
+(some #(> (:critter %) 5) food-journal)
+(some #(> (:critter %) 3) food-journal)
+(some #(and (> (:critter %) 3) %) food-journal)
