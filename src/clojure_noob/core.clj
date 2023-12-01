1f8e6b7a86e1aab1c047455c61e56da3cc8ecee7
Tri <tri.nguyen@gss-sol.com>
2023-12-01 13:44:49 +0700

    Finish chapter 3 and first 3 exercises

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -6,7 +6,7 @@
   [& args]
   (println "I am a teapot"))
 
-(def asym-hobby-body-parts
+(def asym-hobbit-body-parts
   [{:name "head" :size 3}
    {:name "left-eye" :size 1}
    {:name "left-ear" :size 1}
@@ -43,3 +43,50 @@
         (recur remaining
                (into final-body-parts
                      (set [part (matching-part part)])))))))
+
+(defn better-symmetrize-body-parts
+  "Expect a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-hobbit-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining
+               (+ accumulated-size (:size (first remaining))))))))
+
+(hit asym-hobbit-body-parts)
+
+;;;; Exercises
+;; 1.
+(str "Hello " "Mars") ; "Hello Mars"
+(vector 2 4 6) ; [2 4 6]
+(list 2 4 6) ; (2 4 6)
+(hash-map :a 2 :b 4) ; {:b 4, :a 2}
+(hash-set 1 :a :b 2 :b) ; #{1 2 :b :a}
+
+;; 2.
+(defn add-100
+  [n]
+  (+ n 100))
+
+;; 3.
+(defn dec-maker
+  "Create a custom decrementor"
+  [dec-by]
+  ;; (fn [num] (- num dec-by))
+  #(- % dec-by)
+  )
+
+(def dec9 (dec-maker 9))
+
+(dec9 10) ; 1
